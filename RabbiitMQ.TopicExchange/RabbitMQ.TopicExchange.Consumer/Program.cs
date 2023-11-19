ab585653400e62915ae97da922efdc220a1ddfae
Eren Çiçek <117859063+erencicekk25@users.noreply.github.com>
2023-11-20 01:38:31 +0300

    topicExchange

---
+++ RabbiitMQ.TopicExchange/RabbitMQ.TopicExchange.Consumer/Program.cs
@@ -0,0 +1,29 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://wylxhthv:9gEUm_Hi-PGavdDsheGhvL106sliPAi5@shark.rmq.cloudamqp.com/wylxhthv");
+
+using var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+
+channel.BasicQos(0, 1, false);
+var consumer = new EventingBasicConsumer(channel);
+
+var queueName = channel.QueueDeclare().QueueName;
+
+var routeKey = "*.Error.*";
+channel.QueueBind(queueName, "log-topic", routeKey);
+
+channel.BasicConsume(queueName, false, consumer);
+Console.WriteLine("Loglar dinleniyor...");
+
+consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+{
+    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+    Console.WriteLine("Gelen Mesaj: " + message);
+    channel.BasicAck(e.DeliveryTag, false);
+};
+Console.ReadLine();
---
+++ RabbiitMQ.TopicExchange/RabbitMQ.TopicExchange.Publisher/Program.cs
@@ -0,0 +1,37 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://wylxhthv:9gEUm_Hi-PGavdDsheGhvL106sliPAi5@shark.rmq.cloudamqp.com/wylxhthv");
+
+using var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+
+channel.ExchangeDeclare("log-topic", durable: true, type: ExchangeType.Topic);
+
+
+Random rnd = new Random();
+
+Enumerable.Range(1, 50).ToList().ForEach(x =>
+{
+
+    LogNames log1 = (LogNames)rnd.Next(1, 5);
+    LogNames log2 = (LogNames)rnd.Next(1, 5);
+    LogNames log3 = (LogNames)rnd.Next(1, 5);
+    var routeKey = $"{log1}.{log2}.{log3}";
+
+    string message = $"log-type: {log1}-{log2}-{log3}";
+    var messageBody = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish("log-topic", routeKey, null, messageBody);
+
+    Console.WriteLine($"Log gönderilmiştir :  {message}");
+});
+Console.ReadLine();
+public enum LogNames
+{
+    Critical = 1,
+    Error = 2,
+    Warning = 3,
+    Info = 4
+}
